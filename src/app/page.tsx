cb18e3fdf8ca420a4e200dc3b15810fb1e7f540f
katashafar <shafar032@gmail.com>
2023-06-27 22:18:04 +0700

    add gallery, add img in hero comp & change page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import Hero from "@/components/hero";
 import Content from "@/components/content";
 import Testimonial from "@/components/testimonial";
+import Gallery from "@/components/gallery";
 export default function Home() {
   return (
     <main>
       <Hero />
       <Content />
+      <Gallery />
       <Testimonial />
     </main>
   );
